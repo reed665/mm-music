c2823452364d63150bf4c240a64ffc97917605fd
Alexander Buiko <reed665@hotmail.com>
2018-09-15 14:19:43 +0300

    do all stuff

--- src/index.js
+++ src/index.js
@@ -1,13 +1,70 @@
-// const jsmediatags = require('jsmediatags')
-// const fs = require('fs')
+const fs = require('fs')
+const path = require('path')
+const rimraf = require('rimraf')
 const mp3Files = require('./mp3Files')
+const mediaTags = require('./mediaTags')
 
 const mmMusicInput = 'E:/mm-music-input/'
-// const mmMusicOutput = 'E:/mm-music-output/'
+const mmMusicOutput = 'E:/mm-music-output/'
 
-mp3Files(mmMusicInput)
-  .then(files => {
-    console.log(files)
+const clearFolder = folder => {
+  return new Promise((resolve, reject) => {
+    rimraf(folder, err => {
+      if (err) {
+        reject(err)
+        return
+      }
+      fs.mkdirSync(folder)
+      resolve()
+    })
+  })
+}
+
+const tagsPromises = files => {
+  if (!files || !files.length) {
+    console.log('mp3 files not found')
+    return
+  }
+  return Promise.all(files.map(mediaTags))
+}
+
+const simplifyMusicFileData = musicFilesData => {
+  return musicFilesData.map(({ filename, tags }) => {
+    const { artist, year, album } = tags
+    const albumFolder = [artist, year, album].join(' - ')
+    return { filename, folder: albumFolder }
+  })
+}
+
+const getAlbumFolders = simpleData => {
+  const albumFolders = []
+  for (const item of simpleData) {
+    const { folder } = item
+    if (albumFolders.includes(folder)) continue
+    albumFolders.push(folder)
+  }
+  return albumFolders
+}
+
+const createAlbumFoldersSync = (simpleData, mmMusicOutput) => {
+  const albumFolders = getAlbumFolders(simpleData)
+  albumFolders.forEach(folder => fs.mkdirSync(mmMusicOutput + folder))
+}
+
+const copyFiles = (simpleData, destination) => {
+  simpleData.forEach(({ filename, folder }) => {
+    fs.createReadStream(filename).pipe(fs.createWriteStream(path.join(destination, folder, path.basename(filename))))
+  })
+}
+
+Promise.resolve()
+  .then(() => clearFolder(mmMusicOutput))
+  .then(() => mp3Files(mmMusicInput))
+  .then(tagsPromises)
+  .then(simplifyMusicFileData)
+  .then(simpleData => {
+    createAlbumFoldersSync(simpleData, mmMusicOutput)
+    copyFiles(simpleData, mmMusicOutput)
   })
   .catch(err => {
     console.error(err)
---
+++ src/mediaTags.js
@@ -0,0 +1,19 @@
+const jsmediatags = require('jsmediatags')
+
+function mediaTags (filename) {
+  return new Promise((resolve, reject) => {
+    new jsmediatags.Reader(filename)
+      .read({
+        onSuccess: ({ tags }) => {
+          const { artist, year, album } = tags
+          resolve({
+            filename,
+            tags: { artist, year, album }
+          })
+        },
+        onError: err => reject(err)
+      })
+  })
+}
+
+module.exports = mediaTags
